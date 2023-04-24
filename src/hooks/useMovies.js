696d3654e1ca7c00a7c8c64c5ab4332c59259e31
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-04-24 14:23:59 -0400

    Bug fixed

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -23,7 +23,7 @@ export function useMovies({ search, sort }) {
    }, [])
 
    const sortedMovies = useMemo(() => {
-      return sort
+      return sort && movies?.length > 0
          ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
          : movies
    }, [sort, movies])
